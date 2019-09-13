dad5d5c915fcb6a097bbfaf1a51dc5dfef083d40
dosisod <39638017+dosisod@users.noreply.github.com>
2019-09-12 22:54:43 -0700

    added string array link enumeration functionality

--- links.py
+++ links.py
@@ -3,24 +3,14 @@ import re
 
 #given a url with {{}} numbering, explode (enumerate) all possible links
 def explode(url):
-	matches=re.findall(r"\{\{[0-9]+\-[0-9]+\}\}", url)
+	matches=re.findall(r"\{\{(.[^\{\}]*?\|.*?|[0-9]+\-[0-9]+)\}\}", url)
 	splited=re.split(r"\{\{.*?\}\}", url)
 	print(matches, splited)
 
 	if matches!=[] and len(splited)>len(matches):
 		fragments=[] #2d array of each param enumeration
 		for param in matches:
-			param=param[2:-2] #strip the "{{}}" characters off
-
-			tmp=param.split("-")
-			start=int(tmp[0])
-			end=int(tmp[1])
-
-			current=[] #current fragments
-			for n in range(end-start):
-				current.append(start+n) #build out fragments
-
-			fragments.append(current)
+			fragments.append(parse(param))
 
 		#create all possible combos from given lists
 		combos=list(itertools.product(*fragments))
@@ -40,3 +30,19 @@ def explode(url):
 	else:
 		#url is not syntaxed, put it in an arrya then return it
 		return [url]
+
+#given a single {{}} expression, auto identify the possible combos
+def parse(expr):
+	if "|" in expr: #string array representation
+		return expr.split("|")
+
+	else: #number range representation
+		tmp=expr.split("-")
+		start=int(tmp[0])
+		end=int(tmp[1])
+
+		current=[]
+		for n in range(end-start):
+			current.append(start+n) #build out fragments from starting point to end
+
+		return current
